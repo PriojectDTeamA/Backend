dc612afaa14c4492caedf0cf09690fc0da9d3874
Martijn Straathof <martijnfs@me.com>
2022-04-21 12:41:37 +0200

    New response login controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,20 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Backend.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Newtonsoft.Json;
+
+
+
 namespace Backend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class LoginController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+    private MySqlDataReader myReader;
+    private DataTable table { get; set; }
+    private string sqlDataSource { get; set; }
+    public LoginController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        // create connection
+        sqlDataSource = _configuration.GetConnectionString("DbCon"); // DbCon moet nog geadd worden in appsettings onder connectionstring
+        table = new DataTable();
+    }
+
     [HttpPost(Name = "Login")]
     public JsonResult Post([FromBody] Login l)
     {
+
+
         string username = l.username;
         string password = l.password;
         string userID = "1";
         var result = new { userID };
-        // TODO: Run de code op basis van het project ID
 
-        return new JsonResult(new Response { Status = "Succes", Message = userID });
+        string query = @"SELECT * FROM Users WHERE username=@username";
+        using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+        {
+            mycon.Open();
+            using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+            {
+                myCommand.Parameters.AddWithValue("@username", username);
+                myReader = myCommand.ExecuteReader();
+                table.Load(myReader);
+
+                myReader.Close();
+                mycon.Close();
+            }
+        }
+        string JSONString = string.Empty;
+        JSONString = JsonConvert.SerializeObject(table);
+        return new JsonResult(new Response { Status = "Succes", Message = JSONString });
     }
 }
