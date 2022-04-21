a483dab432e1db93d1ae17b1e4dd83caed857d18
Martijn Straathof <martijnfs@me.com>
2022-04-21 11:35:55 +0200

    added login api

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    [HttpPost(Name = "Login")]
+    public JsonResult Post([FromBody] Login l)
+    {
+        string username = l.username;
+        string password = l.password;
+        string userID = "1";
+        var result = new { userID };
+        // TODO: Run de code op basis van het project ID
+
+        return new JsonResult(result);
+    }
+}
---
+++ models/Login.cs
@@ -0,0 +1,7 @@
+namespace Backend.Controllers;
+
+public class Login
+{
+    public string username { get; set; }
+    public string password { get; set; }
+}
