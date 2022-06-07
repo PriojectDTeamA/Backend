df79ed5b513cf64ae209cde1f1c9e8a81f6b65f4
Damian <25197071+TheDenizen@users.noreply.github.com>
2022-06-07 13:26:33 +0200

    2

--- models/DockerBuilder.cs
+++ models/DockerBuilder.cs
@@ -259,13 +259,10 @@ public class JavaBuilder : DockerBuilder
        // "RUN npm install",
        // "COPY . .",
        // "CMD [ \"node\", \".\" ]",
-
-        // Base Alpine Linux based image with OpenJDK JRE only
-           "FROM openjdk:8-jre-alpine",
-        //copy application WAR (with libraries inside)
-        "COPY target/spring-boot-*.war /app.war",
-        // specify default command
-        "CMD [\"/usr/bin/java\", \"-jar\", \"-Dspring.profiles.active=test/\", \"/app.war\"]",
+       "FROM openjdk:17",
+       "COPY ./out/production/DockerHelloJava/ /tmp\"",
+       "WORKDIR /tmp",
+       "ENTRYPOINT [\"java\",\"HelloWorld\"]"
     })
     {
 
--- templates/java/Program.java
+++ templates/java/Program.java
@@ -1,5 +1,5 @@
-class Program{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
