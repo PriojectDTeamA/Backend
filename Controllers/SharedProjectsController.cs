2fc6dab8da004ef1f8829b2129f349a7bcb45d9a
TeuntjeHR <1007734@hr.nl>
2022-06-16 12:23:40 +0200

    removed if-else statement with single query

--- Controllers/SharedProjectsController.cs
+++ Controllers/SharedProjectsController.cs
@@ -32,7 +32,14 @@ public class SharedProj : ControllerBase
     //Function for Modifying and adding data to database
     public JsonResult SetSharedProject([FromBody] SharedProject proj)
     {
-        string query = @"SELECT * FROM RecentProjects WHERE UserID=@UserID AND ProjectID=@ProjectID";
+        //make the timestamp
+        string Timestamp = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+        //make new database value or update an existing one
+        string query = @"INSERT INTO RecentProjects (ProjectID,UserID,Timestamp) 
+                        VALUES (@ProjectID,@UserID,@Timestamp)
+                        ON DUPLICATE KEY UPDATE Timestamp=@Timestamp;";
+
         using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
         {
             //establishing connection with database
@@ -42,6 +49,7 @@ public class SharedProj : ControllerBase
                 //Replacing values with data
                 myCommand.Parameters.AddWithValue("@UserID", proj.UserID);
                 myCommand.Parameters.AddWithValue("@ProjectID", proj.ProjectID);
+                myCommand.Parameters.AddWithValue("@Timestamp", Timestamp);
 
                 //running the Query
                 myReader = myCommand.ExecuteReader();
@@ -50,44 +58,6 @@ public class SharedProj : ControllerBase
                 //closing connection with database
                 myReader.Close();
                 mycon.Close();
-            }
-        }
-
-        //creating the timestamp of current time and date
-        string Timestamp = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
-
-        //checking if returned data is empty
-        if (table.Rows.Count == 0)
-        {
-            //Query for creating new line in DB when user hasn't joined the project before
-            query = @"INSERT INTO RecentProjects (ProjectID, UserID, Timestamp) VALUES (@ProjectID, @UserID, @Timestamp)";
-        }
-
-        else
-        {
-            //Query for updating timestamp in table when the user joins the room once more
-            query = @"UPDATE RecentProjects SET Timestamp=@Timestamp WHERE UserID=@UserId AND ProjectID=@ProjectID";
-        }
-
-        //obtaining connection string for the database
-        using (MySqlConnection DBCon = new MySqlConnection(sqlDataSource))
-        {
-            //Establishing Connection with Database
-            DBCon.Open();
-            using (MySqlCommand myCommand = new MySqlCommand(query, DBCon))
-            {
-                //Replacing values with data
-                myCommand.Parameters.AddWithValue("@UserID", proj.UserID);
-                myCommand.Parameters.AddWithValue("@ProjectID", proj.ProjectID);
-                myCommand.Parameters.AddWithValue("@Timestamp", Timestamp);
-
-                //Running the query
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-
-                //Closing connection with Database
-                myReader.Close();
-                DBCon.Close();
             }
         }
         // In case we want to add state checking
@@ -101,10 +71,11 @@ public class SharedProj : ControllerBase
     public JsonResult getAllProjectsOfUser(int UserID)
     {
         //Query for the Shared projects ordered by timestamp. Joined with Projects table for "Language"
-        string query = @"SELECT * FROM Projects INNER JOIN RecentProjects ON 
-                            RecentProjects.ProjectID=Projects.ID 
-                            WHERE NOT Projects.owner=@UserID AND RecentProjects.UserID=@UserID 
-                            ORDER BY RecentProjects.Timestamp DESC";
+        string query = @"SELECT * 
+                        FROM Projects 
+                        INNER JOIN RecentProjects ON RecentProjects.ProjectID=Projects.ID 
+                        WHERE NOT Projects.owner=@UserID AND RecentProjects.UserID=@UserID 
+                        ORDER BY RecentProjects.Timestamp DESC";
         using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
         {
             //establishing connection with the database
